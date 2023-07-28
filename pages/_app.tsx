2434dc212f7625929fe7ddc7d58e3bfae1710a1e
Huyllee <moriarty1807@gmail.com>
2023-07-28 17:54:35 +0700

    create navbar and sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { useEffect, useState } from 'react';
 
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
    const [isSSR, setIsSSR] = useState(true);
 
@@ -13,7 +16,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
    return (
       <div>
-         <Component {...pageProps} />
+         <Navbar />
+         <div className="flex gap-6 md:gap-20">
+            <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+               <Sidebar />
+            </div>
+            <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+               <Component {...pageProps} />
+            </div>
+         </div>
       </div>
    );
 };
