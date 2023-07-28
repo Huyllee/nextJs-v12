7c0620a3797f24c6ce4dd9d5a4e394b3711559e6
Huyllee <moriarty1807@gmail.com>
2023-07-28 11:29:13 +0700

    setup tailwindCSS and prettier

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { useEffect, useState } from 'react';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+   const [isSSR, setIsSSR] = useState(true);
+
+   useEffect(() => {
+      setIsSSR(false);
+   }, []);
+
+   if (isSSR) return null;
+
+   return (
+      <div>
+         <Component {...pageProps} />
+      </div>
+   );
+};
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+   return (
+      <div className="">
+         Tóp Tóp
+         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+   );
+};
+
+export default Home;
