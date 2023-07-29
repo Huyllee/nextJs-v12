508a83e38d94ad21ab542b51ecffe5c82653bc64
Huyllee <moriarty1807@gmail.com>
2023-07-29 11:21:06 +0700

    login using google account

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,33 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { useEffect, useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-   const [isSSR, setIsSSR] = useState(true);
+    const [isSSR, setIsSSR] = useState(true);
 
-   useEffect(() => {
-      setIsSSR(false);
-   }, []);
+    useEffect(() => {
+        setIsSSR(false);
+    }, []);
 
-   if (isSSR) return null;
+    if (isSSR) return null;
 
-   return (
-      <div>
-         <Navbar />
-         <div className="flex gap-6 md:gap-20">
-            <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-               <Sidebar />
+    return (
+        <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+            <Navbar />
+            <div className="flex gap-6 md:gap-20">
+                <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+                    <Sidebar />
+                </div>
+                <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+                    <Component {...pageProps} />
+                </div>
             </div>
-            <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-               <Component {...pageProps} />
-            </div>
-         </div>
-      </div>
-   );
+        </GoogleOAuthProvider>
+    );
 };
 
 export default MyApp;
